431fe944442397c050dc05c5a8c45855318f3070
qiuming <972040417@qq.com>
2020-04-06 23:28:18 +0800

    test auto build

--- src/pages/Menu.vue
+++ src/pages/Menu.vue
@@ -2,10 +2,10 @@
     <div>
         <ul>
             <li>
-                <router-link to="/menu/submenu">子菜单</router-link>
+                <router-link to="/menu/submenu">测试子菜单</router-link>
             </li>
             <li>
-                <router-link to="/menu/submenu1">子菜单1</router-link>
+                <router-link to="/menu/submenu1">测试子菜单1</router-link>
             </li>
         </ul>
         <router-view></router-view>
